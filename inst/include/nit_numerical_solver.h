8b36210d99b317a35647114962409690e53b85b2
Joanna Simms <joanna.x.simms@helsinki.fi>
2025-01-17 11:50:19 +0200

    changed the parameters so the root cost per zeta is only acting on the last term now

--- inst/include/nit_numerical_solver.h
+++ inst/include/nit_numerical_solver.h
@@ -50,7 +50,7 @@ namespace phydro{
       
       double jmax = n_leaf * par_photosynth.a_jmax;
       // NOTE: Format chosen as it is as close to the equations as possible
-      double costs = (par_cost.alpha * jmax) / (par_cost.root_cost_per_zeta * zeta) + par_cost.gamma * dpsi * dpsi + par_cost.root_cost_per_zeta * zeta;
+      double costs = (par_cost.alpha / zeta * jmax) + par_cost.gamma * dpsi * dpsi + par_cost.root_cost_per_zeta * zeta;
       
       double profit = aj.a - costs;
       
